f892274ba1adcff83c9c993ba14bcdc93a3c9eb2
ibaky <alexis76360@hotmail.fr>
2024-11-23 00:36:59 +0100

    mise a jours de numero telephone

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,7 +8,7 @@ export default function Navbar() {
   return (
     <nav className={styles.navbar}>
       <div className={styles.logo}>
-        {/* Intégration du logo avec le chemin correct */}
+        {/* Logo à gauche */}
         <Link href="/">
           <Image
             src="/image/logo.jpg" // Chemin vers le fichier logo.jpg
@@ -16,36 +16,43 @@ export default function Navbar() {
             width={80} // Largeur de l'image
             height={80} // Hauteur de l'image
             priority // Chargement prioritaire pour le logo
+            
           />
         </Link>
       </div>
-      <ul className={styles.navLinks}>
-        <li>
-          <Link href="/" className={styles.navLink}>
-            Accueil
-          </Link>
-        </li>
-        <li>
-          <Link href="/apropos" className={styles.navLink}>
-            À propos
-          </Link>
-        </li>
-        <li>
-          <Link href="/contact" className={styles.navLink}>
-            Contact
-          </Link>
-        </li>
-        <li>
-          <Link href="/premium" className={styles.navLink}>
-            Premium
-          </Link>
-        </li>
-        <li>
-          <Link href="/visite" className={styles.navLink}>
-            Visite
-          </Link>
-        </li>
-      </ul>
+      <div className={styles.navContent}>
+        {/* Numéro de téléphone au-dessus des liens */}
+        <div className={styles.phoneContainer}>
+          📞 <span className={styles.phoneNumber}>+33 6 12 34 56 78</span>
+        </div>
+        <ul className={styles.navLinks}>
+          <li>
+            <Link href="/" className={styles.navLink}>
+              Accueil
+            </Link>
+          </li>
+          <li>
+            <Link href="/apropos" className={styles.navLink}>
+              À propos
+            </Link>
+          </li>
+          <li>
+            <Link href="/contact" className={styles.navLink}>
+              Contact
+            </Link>
+          </li>
+          <li>
+            <Link href="/premium" className={styles.navLink}>
+              Premium
+            </Link>
+          </li>
+          <li>
+            <Link href="/visite" className={styles.navLink}>
+              Visite
+            </Link>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 }
