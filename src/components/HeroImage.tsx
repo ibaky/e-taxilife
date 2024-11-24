6901462bc4ec6403bdb3ed7c78d0d51d06f75a22
ibaky <alexis76360@hotmail.fr>
2024-11-24 04:15:16 +0100

    ajouts de texte sur img

--- src/components/HeroImage.tsx
+++ src/components/HeroImage.tsx
@@ -7,14 +7,20 @@ import styles from './HeroImage.module.css';
 const HeroImage: React.FC = () => {
   return (
     <div className={styles.imageContainer}>
+      {/* Image */}
       <Image
-        src="/image/taxilife.jpg" // Ton image
+        src="/image/taxilife.jpg"
         alt="Une belle vue de TaxiLife"
-        layout="responsive" // L'image sera responsive
-        width={1920} // Largeur par défaut
-        height={400} // Hauteur ajustée
+        layout="responsive"
+        width={1920}
+        height={400}
         priority
       />
+
+      {/* Texte au centre */}
+      <div className={styles.centeredText}>
+        Disponible 24h/7j
+      </div>
     </div>
   );
 };
