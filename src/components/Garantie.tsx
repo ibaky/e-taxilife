39b9961f3890b05c35eb9174f3be6dfa24649b36
shawgoon <alban.havard@aliceadsl.fr>
2024-12-02 14:45:32 +0100

    rajot de la nouvelle section garantie

--- src/components/Garantie.tsx
+++ src/components/Garantie.tsx
@@ -1,14 +1,16 @@
 // src/components/Garentie.tsx
 import React from 'react';
-import styles from './Garantie.module.css'; // Import des styles CSS Modules
+import styles from './Garantie.module.css';  // Importation du fichier CSS
 
 const Garantie: React.FC = () => {
   return (
-    <section className={styles.garentieSection}>
-      <div className={styles.container}></div>
+    <section className={styles.garantieContainer}>
+      {/* Ajouter une image à l'intérieur du conteneur */}
+      <img src="/image/stop.jpg" alt="Image Description" className={styles.garentieImage} />
     </section>
   );
 };
 
 export default Garantie;
+
 
