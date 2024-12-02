154ba986328a790b51b520e7b4d8b5769154edc7
shawgoon <alban.havard@aliceadsl.fr>
2024-12-02 16:15:27 +0100

    Ajout de la fonctionnalité de garantie et animation

--- src/components/Garantie.tsx
+++ src/components/Garantie.tsx
@@ -1,16 +1,33 @@
-// src/components/Garentie.tsx
 import React from 'react';
-import styles from './Garantie.module.css';  // Importation du fichier CSS
+import { Player } from '@lottiefiles/react-lottie-player'; // Import Lottie
+import styles from './Garantie.module.css'; // Importation du fichier CSS
 
 const Garantie: React.FC = () => {
   return (
     <section className={styles.garantieContainer}>
-      {/* Ajouter une image à l'intérieur du conteneur */}
-      <img src="/image/stop.jpg" alt="Image Description" className={styles.garentieImage} />
+      <img src="/image/stop.jpg" alt="Description de l'image" className={styles.garentieImage} />
+      
+      <div className={styles.garantieEnfant}>
+        {/* Texte ajouté à gauche dans le container enfant */}
+        <div className={styles.garantieText}>
+          Nous vous <br /> garantissons
+        </div>
+
+        {/* Animation ajoutée à droite du texte */}
+        <div className={styles.animationContainer}>
+          <Player
+            autoplay
+            loop
+            src="/animations/wired-outline-939-taxi-front-hover-pinch.json" // Chemin vers l'animation
+            style={{ height: '50px', width: '50px' }} // Ajustez la taille de l'animation ici
+          />
+        </div>
+      </div>
     </section>
   );
 };
 
 export default Garantie;
+
 
 
