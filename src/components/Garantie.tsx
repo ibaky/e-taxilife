5ddb2c2bc9577a67076af8659f68b438c78f7930
shawgoon <alban.havard@aliceadsl.fr>
2024-12-16 11:53:20 +0100

    rajout d'amination

--- src/components/Garantie.tsx
+++ src/components/Garantie.tsx
@@ -13,21 +13,52 @@ const Garantie: React.FC = () => {
           Nous vous <br /> garantissons
         </div>
 
-        {/* Animation ajoutée à droite du texte */}
-        <div className={styles.animationContainer}>
+        {/* Conteneur pour l'animation et le texte à côté */}
+        <div className={styles.animationTextContainer}>
           <Player
             autoplay
             loop
-            src="/animations/wired-outline-939-taxi-front-hover-pinch.json" // Chemin vers l'animation
-            style={{ height: '50px', width: '50px' }} // Ajustez la taille de l'animation ici
+            src="/animations/wired-outline-860-electric-car-hover-pinch.json" // Chemin vers l'animation
+            style={{ height: '70px', width: '70px' }} // Ajustez la taille de l'animation ici
           />
+          <div className={styles.textNextToAnimation}>
+            Taxi Electrique
+          </div>
         </div>
+
+        {/* Nouvelle animation "Transport pour tous" sous la première animation */}
+        <div className={styles.animationTransportContainer}>
+          <Player
+            autoplay
+            loop
+            src="/animations/wired-outline-651-disabled-person-hover-pinch.json" // Assurez-vous d'avoir le bon chemin pour l'animation
+            style={{ height: '70px', width: '70px' }} // Ajustez la taille de la nouvelle animation
+          />
+          <div className={styles.textNextToAnimation}>
+            Transport pour tous
+          </div>
+        </div>
+
+        {/* Nouvelle animation pour "Standard disponible 24h/7" */}
+        <div className={styles.animationStandardContainer}>
+          <Player
+            autoplay
+            loop
+            src="animations/wired-outline-405-non-stop-24-h-hover-spin (1).json" // Assurez-vous d'avoir l'animation
+            style={{ height: '70px', width: '70px' }} // Ajustez la taille de la nouvelle animation
+          />
+          <div className={styles.textNextToAnimation}>
+            Standard disponible 24h/7
+          </div>
+        </div>
+        
       </div>
     </section>
   );
 };
 
 export default Garantie;
+
 
 
 
